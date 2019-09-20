264c146d9191a55e75e8597df23d1bc97d2b358d
gccpacman <gccpacman@gmail.com>
2019-09-20 16:52:06 +0800

    k8s vagrantfile and ansible playbook added

---
+++ kubernetes-setup/pull-k8s-image.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+KUBE_VERSION=v1.15.3
+KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.3.10
+CORE_DNS_VERSION=1.3.1
+
+GCR_URL=k8s.gcr.io
+ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/google_containers
+
+images=(kube-proxy:${KUBE_VERSION}
+kube-scheduler:${KUBE_VERSION}
+kube-controller-manager:${KUBE_VERSION}
+kube-apiserver:${KUBE_VERSION}
+pause:${KUBE_PAUSE_VERSION}
+etcd:${ETCD_VERSION}
+coredns:${CORE_DNS_VERSION})
+
+
+for imageName in ${images[@]} ; do
+  docker pull $ALIYUN_URL/$imageName
+  docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
+  docker rmi $ALIYUN_URL/$imageName
+done
