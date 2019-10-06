dd45c41bb1e697db28a1f396deeb8fb846e76bd8
gccpacman <gccpacman@gmail.com>
2019-10-06 11:17:38 +0800

    add ansible command for tiller image pulling

--- kubernetes-setup/pull-k8s-image.sh
+++ kubernetes-setup/pull-k8s-image.sh
@@ -17,9 +17,16 @@ pause:${KUBE_PAUSE_VERSION}
 etcd:${ETCD_VERSION}
 coredns:${CORE_DNS_VERSION})
 
-
 for imageName in ${images[@]} ; do
   docker pull $ALIYUN_URL/$imageName
   docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
   docker rmi $ALIYUN_URL/$imageName
 done
+
+HELM_TILLER_VERSION=v2.14.3
+HELM_TILLER_ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:${HELM_TILLER_VERSION}
+HELM_TILLER_URL=gcr.io/kubernetes-helm/tiller:${HELM_TILLER_VERSION}
+
+docker pull $HELM_TILLER_ALIYUN_URL
+docker tag $HELM_TILLER_ALIYUN_URL $HELM_TILLER_URL
+docker rmi $HELM_TILLER_ALIYUN_URL
